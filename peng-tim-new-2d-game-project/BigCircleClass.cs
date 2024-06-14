b1c8cd9cc00fe7a42979fa3fb451254dd8b92193
h <mlgdashyt@gmail.com>
2024-06-13 20:09:05 -0400

    WIP code

---
+++ peng-tim-new-2d-game-project/BigCircleClass.cs
@@ -0,0 +1,35 @@
+﻿using Raylib_cs;
+using System.Numerics;
+
+public class BigCircleClass
+{
+    public Vector2 Position;
+    public float Radius;
+    public Vector2 Velocity;
+    public float Gravity;
+
+    public BigCircleClass(Vector2 position, float radius, float gravity)
+    {
+        Position = position;
+        Radius = radius;
+        Velocity = Vector2.Zero;
+        Gravity = gravity;
+    }
+
+    public void Update()
+    {
+        Position.Y += Velocity.Y;
+        Velocity.Y += Gravity;
+    }
+
+    public void Draw()
+    {
+        Raylib.DrawCircle((int)Position.X, (int)Position.Y, Radius, Color.Red);
+    }
+
+    public bool CheckCollisionWithPlayer(Vector2 playerPosition, Vector2 playerSize)
+    {
+        Rectangle playerRect = new Rectangle(playerPosition.X, playerPosition.Y, playerSize.X, playerSize.Y);
+        return Raylib.CheckCollisionCircleRec(Position, Radius, playerRect);
+    }
+}
--- peng-tim-new-2d-game-project/Program.cs
+++ peng-tim-new-2d-game-project/Program.cs
@@ -1,10 +1,66 @@
-﻿namespace peng_tim_new_2d_game_project
+﻿using Raylib_cs;
+using System.Numerics;
+using Color = Raylib_cs.Color;
+
+public class Program
 {
-    internal class Program
+    static string title = "Simple Collision Game";
+    static int screenWidth = 800;
+    static int screenHeight = 600;
+    static int targetFps = 60;
+
+    // player variables
+    static Vector2 position;
+    static Vector2 size = new Vector2(40, 40); // player size
+    static Vector2 velocity;
+    static Vector2 gravity = new Vector2(0, 100); // gravity vector
+
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        Raylib.InitWindow(screenWidth, screenHeight, title);
+        Raylib.SetTargetFPS(targetFps);
+        Setup();
+
+        while (!Raylib.WindowShouldClose())
+        {           
+            Draw();
         }
+
+        Raylib.CloseWindow();
+    }
+
+    static void Setup()
+    {
+        // initialize player position at center
+        position = new Vector2(screenWidth / 2, screenHeight / 2);
+        velocity = Vector2.Zero;
+    }
+
+
+    static void Draw()
+    {
+        Raylib.BeginDrawing();
+        Raylib.ClearBackground(Color.RayWhite);
+
+        // draw player
+        Raylib.DrawRectangle((int)position.X, (int)position.Y, (int)size.X, (int)size.Y, Color.DarkGreen);
+
+        Raylib.EndDrawing();
+    }
+
+
+
+    static void CheckScreenBoundaries()
+    {
+        // ensure player stays within screen boundaries
+        if (position.X < 0)
+            position.X = 0;
+        else if (position.X > screenWidth - size.X)
+            position.X = screenWidth - size.X;
+
+        if (position.Y < 0)
+            position.Y = 0;
+        else if (position.Y > screenHeight - size.Y)
+            position.Y = screenHeight - size.Y;
     }
 }
